94a79eb1984b0f034393b24482e56887a9bebff1
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2023-05-03 00:08:12 -0300

    class 2 finished config env and module user added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./env/.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = app.get(ConfigService).get('PORT');
+
+  app.enableCors(CORS);
+  // console.log(process.cwd());
+  app.setGlobalPrefix('api');
+
+  await app.listen(port, () => {
+    console.log(`Application running on: ${port}`);
+  });
 }
+
 bootstrap();
